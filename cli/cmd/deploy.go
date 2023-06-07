a16ba7ec9062d8dbec8b5f25bc6b6d2dc06dccc7
Halil Tezcan KARABULUT <unity@themaker.cyou>
2023-06-08 01:41:10 +0300

    feat(cli): initial commit

---
+++ cli/cmd/deploy.go
@@ -0,0 +1,147 @@
+package cmd
+
+import (
+	"archive/zip"
+	"bytes"
+	"fmt"
+	"github.com/spf13/cobra"
+	"io"
+	"math/rand"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+var deployCmd = &cobra.Command{
+	Use:   "deploy",
+	Short: "Deploy the build output to the NitroDeployer server",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return deploy(args)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(deployCmd)
+}
+
+func generateTempFilename() string {
+	rand.Seed(time.Now().UnixNano())
+	randID := fmt.Sprintf("%016x", rand.Uint64())
+	return filepath.Join(os.TempDir(), "nitro-deployment-"+randID)
+}
+
+func uploadFile(filePath, url string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile("file", filepath.Base(filePath))
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(part, file)
+
+	err = writer.Close()
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+	return nil
+}
+
+func zipDirectory(dirPath, zipFilePath string) error {
+	// To zip a directory, we need to:
+	// 1. Create a zip file
+	// 2. Walk the directory and add each file to the zip file
+	// 3. Close the zip file
+	zipFile, err := os.Create(zipFilePath)
+	if err != nil {
+		return err
+	}
+	defer zipFile.Close()
+
+	zipWriter := zip.NewWriter(zipFile)
+	defer zipWriter.Close()
+
+	err = filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			// add a trailing slash for creating dir
+			path = fmt.Sprintf("%s%c", path, os.PathSeparator)
+			_, err = zipWriter.Create(path)
+			return err
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		zipFile, err := zipWriter.Create(path)
+		if err != nil {
+			return err
+		}
+
+		_, err = io.Copy(zipFile, file)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func deploy(args []string) error {
+	// Create zip file from build output
+	zipFilename := generateTempFilename()
+	err := zipDirectory(".output", zipFilename)
+	if err != nil {
+		return err
+	}
+
+	// Upload zip file to server
+	err = uploadFile(zipFilename, serverURL+"/upload")
+	if err != nil {
+		return err
+	}
+
+	// Delete zip file
+	err = os.Remove(zipFilename)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ cli/cmd/root.go
@@ -0,0 +1,84 @@
+/*
+Copyright © 2023 Halil Tezcan KARABULUT <unity@themaker.cyou>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "nitro-deploy",
+	Short: "nitro-deploy is a cli tool for deploying nitro apps blazing fast!",
+	Long:  `Nitro Deployer is a cli tool that helps you to deploy your nitro apps to our cloud. Blah blah`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+var (
+	targetFileName string
+	serverURL      string
+)
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.nitro-deploy.yaml)")
+
+	rootCmd.PersistentFlags().StringVar(&serverURL, "server", "http://localhost:8080", "URL of the NitroDeployer server")
+	rootCmd.PersistentFlags().StringVar(&targetFileName, "filename", ".output", "Filename of the build output file")
+
+	if err := viper.BindPFlag("server", rootCmd.PersistentFlags().Lookup("server")); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	if err := viper.BindPFlag("filename", rootCmd.PersistentFlags().Lookup("filename")); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".nitro-k8s-deployer-cli" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".nitro-deploy")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
