82524a3cf82ee312c54b4b3f4fd09efb0535b188
Halil Tezcan KARABULUT <unity@themaker.cyou>
2023-06-11 08:45:37 +0300

    fix(server): do not crash if config doesn't exist

--- server/internal/utils/config/config.go
+++ server/internal/utils/config/config.go
@@ -17,9 +17,13 @@ func Init() {
 func InitViper() {
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")
-	err := viper.ReadInConfig()
-	if err != nil {
-		log.Fatal(err)
+	// Set default values
+	viper.SetDefault("myKey", "defaultValue")
+	if err := viper.ReadInConfig(); err != nil {
+		// If the config file is not found, do not return an error
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			log.Fatalf("Failed to read config file: %s", err)
+		}
 	}
 }
 
@@ -28,7 +32,7 @@ func InitDotenv() {
 	if err != nil {
 		// if .env file is not found, ignore the error
 		if _, ok := err.(*os.PathError); !ok {
-			log.Fatal(err)
+			log.Fatalf("Failed to load .env file: %s", err)
 		}
 	}
 }
