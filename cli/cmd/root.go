d0cc529dfb1531dc85c465fa330c1e7af402d164
Halil Tezcan KARABULUT <unity@themaker.cyou>
2023-06-15 06:17:12 +0300

    feat(cli): ability to read from config, project specific configs (#40)

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -5,10 +5,10 @@ package cmd
 
 import (
 	"fmt"
-	"os"
-
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"os"
+	"path/filepath"
 )
 
 var cfgFile string
@@ -26,10 +26,6 @@ var rootCmd = &cobra.Command{
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-var (
-	serverURL string
-)
-
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
@@ -46,14 +42,10 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.deployer.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.bulut.yaml)")
+	rootCmd.PersistentFlags().String("server", "http://localhost:8080", "URL of the Deployer server")
 
-	rootCmd.PersistentFlags().StringVar(&serverURL, "server", "http://localhost:8080", "URL of the Deployer server")
-
-	if err := viper.BindPFlag("server", rootCmd.PersistentFlags().Lookup("server")); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	viper.BindPFlag("server", rootCmd.Flags().Lookup("server"))
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -62,20 +54,59 @@ func initConfig() {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
+		// Check if there is a config in the current directory
+		currentDir, err := os.Getwd()
 		cobra.CheckErr(err)
+		for i := 0; i < 2; i++ {
+			var newDir string
+			var configFound bool
+			configFound, newDir = checkConfigAndUpdateDir(currentDir)
+			if configFound {
+				viper.AddConfigPath(currentDir)
+				currentDir = newDir
+				break
+			}
+			currentDir = newDir
+		}
 
-		// Search config in home directory with name ".nitro-k8s-deployer-cli" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".deployer")
+		cobra.CheckErr(err)
 	}
+
+	// Find home directory.
+	home, err := os.UserHomeDir()
+	cobra.CheckErr(err)
+
+	// Search config in home directory
+	viper.AddConfigPath(home)
+	viper.SetConfigType("yaml")
+	viper.SetConfigName(".bulut")
+
+	// Set defaults
+	viper.SetDefault("server.host", "127.0.0.1")
+	viper.SetDefault("server.port", 8080)
+	viper.SetDefault("server.ssl", false) // TODO: in the production, this should be true
+	viper.SetDefault("config.build-path", ".output")
+	viper.SetDefault("config.entrypoint", "server/index.mjs")
 
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
+	if err := viper.MergeInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
+}
+
+func checkConfigAndUpdateDir(currentDir string) (bool, string) {
+	_, err := os.Stat(currentDir + "/.bulut.yaml")
+	updatedDir := updateDir(currentDir)
+	if err == nil {
+		return true, updatedDir
+	}
+	return false, updatedDir
+}
+
+func updateDir(currentDir string) string {
+	updatedDir, err := filepath.Abs(filepath.Join(currentDir, ".."))
+	cobra.CheckErr(err)
+	return updatedDir
 }
