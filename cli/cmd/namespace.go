644928350c7f4fc639bebbf896e646017908c2c8
Halil Tezcan KARABULUT <unity@themaker.cyou>
2023-07-12 16:53:48 +0300

    fix(cli): fixed create namespace command

--- cli/cmd/namespace.go
+++ cli/cmd/namespace.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/spf13/cobra"
@@ -12,13 +13,13 @@ import (
 	"net/http"
 )
 
-// namespaceCmd represents the namespace command
 var namespaceCmd = &cobra.Command{
 	Use:     "namespace",
 	Aliases: []string{"ns"},
 	Short:   "Manage namespaces",
 	Long:    `Namespace is a way to divide cluster resources between multiple projects.`,
 	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		cmd.SilenceUsage = true
 		return checkLogin()
 	},
 }
@@ -53,8 +54,9 @@ func createNamespaceHandler(args []string) error {
 			return fmt.Errorf("namespace name cannot be empty")
 		}
 	}
-	fmt.Printf("Creating namespace \"%s\"", name)
-	return nil
+	fmt.Printf("Creating namespace \"%s\"\n", name)
+	err := createNamespace(name)
+	return err
 }
 
 func createNamespace(name string) error {
@@ -63,11 +65,17 @@ func createNamespace(name string) error {
 	if err != nil {
 		return err
 	}
-	req, err := http.NewRequest("POST", serverURL+"/api/v1/namespace", nil)
+	body := &bytes.Buffer{}
+	_, err = body.WriteString(fmt.Sprintf(`{"name":"%s"}`, name))
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("POST", serverURL+"/namespace/", body)
 	if err != nil {
 		return err
 	}
 	req.Header.Set("Authorization", apiKey)
+	req.Header.Set("Content-Type", "application/json")
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -93,6 +101,10 @@ func checkLogin() error {
 	serverURL := getServerURL()
 	_, err := getApiKeyForServer(serverURL)
 	if err != nil {
+		if errors.Is(err, ring.ErrNotFound) {
+			err = fmt.Errorf("you are not logged in to %s", serverURL)
+			return err
+		}
 		return err
 	}
 	return nil
